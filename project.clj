3c24a6681fb63aceb89311788c50ba447a4047bc
Richard Möhn <richard.moehn@posteo.de>
2014-11-20 19:10:20 +0100

    Correct project.clj
    The build did fail, although not because a test failed. Instead I had
    overlooked some things about how to configure everything correctly for
    Travis. Hope it works now.

--- project.clj
+++ project.clj
@@ -1,5 +1,7 @@
 (defproject theatralia "0.0.1-SNAPSHOT"
   :description "Cool new project to do things and stuff"
   :dependencies [[org.clojure/clojure "1.6.0"]]
-  :profiles {:dev {:dependencies [[midje "1.6.3"]]}})
-
+  :profiles {:dev {:dependencies
+                   [[midje "1.6.3" :exclusions [org.clojure/clojure]]]
+                   :plugins
+                   [[lein-midje "3.1.3"]]}})
