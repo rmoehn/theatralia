ad1fa3662488bd0b60e51a41640feb41af54c070
Richard Möhn <richard.moehn@posteo.de>
2014-12-23 11:58:05 +0100

    Make the design a bit more palatable
    Roughly put the elements in the right places. Very roughly. There are no
    colours, yet.

--- src/theatralia/welcome_page.clj
+++ src/theatralia/welcome_page.clj
@@ -2,12 +2,14 @@
   (:require [net.cgrand.enlive-html :as html]
             [garden.core :refer [css]]
             [garden.stylesheet :refer [at-font-face]]
+            [garden.units :as u]
             [clojure.string :as string]
             [clojure.java.io :as io]))
 ;;; Credits:
 ;;;  - https://github.com/cgrand/enlive
 ;;;  - https://github.com/swannodette/enlive-tutorial/
 ;;;  - http://clojurewebdev.com/workshop/slides/
+;;;  - http://stackoverflow.com/questions/15193848/i-want-my-label-to-vertically-align-with-my-input-field
 ;;;  - http://www.w3schools.com/css/default.asp
 
 (def ^:private main-html (io/resource "templates/around.html"))
@@ -29,9 +31,6 @@
   [:#main-content] (html/content (welcome-snippet))
   [:#global-tools] (html/content (login-snippet)))
 
-(def css-data [[:html {:font-family "sans-serif"}]
-               [:#theatralia {:font-family "fancy"}]
-               [:#login {:float "right"}]])
 (def ^:private css-map-for-style
   {\R {}
    \B {:font-weight "bold"}
@@ -55,6 +54,20 @@
   (concat
     (build-font-css "my-sans" "/fonts" "LinBiolinum" ["R" "RB" "RI"])
     (build-font-css "my-serif" "/fonts" "LinLibertine" ["R" "RB" "RI" "RBI"])))
+
+(def ^:private css-data
+  (concat
+    font-declaration
+    [[:* {:font-family "my-sans"}]
+     [:.theatralia {:font-family "my-serif" :font-weight "bold"}]
+     [:#main-content {:float "left"}]
+     [:p {:max-width (u/ch 65)}]
+     [:#global-tools {:float "left"}]
+     [:label :input {:display "block"}]
+     [:footer {:clear "both"
+               :text-align "center"
+               :font-size "small"
+               :padding-top (u/ex 2)}]]))
 
 (defn main-css []
   {:status 200
