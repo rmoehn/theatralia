521f2824b8cb3a983c4f834038cdc62a30996e0a
Richard Möhn <richard.moehn@posteo.de>
2015-02-22 14:04:47 +0100

    Add primitive search and response
    Remove all the last-input stuff from routes.clj and add a query route
    that performs a full-text search on the database and returns the result.
    I intend to support full-text search on titles, tags and URIs at once,
    but keep it small for now, supporting only titles.
    
    Add a template that displays a search field and a result list. I wanted
    the search field and the button nicely joined together and copied the
    necessary snippet from the Bootstrap documentation. Unfortunatly it
    doesn't work. I suspect this is because some part of my React setup
    (Kioo?) scatters empty spans in between elements. But this is not
    important, therefore I won't investigate it now.
    
    Remove the om-sync stuff from core.cljs and set up the user interface
    with search field and result list and requesting search results. A few
    things to say here:
    
     - I'm not sure if om-sync will make sense in my case. (Or maybe I just
       don't know how to use it properly.) For now things appear to be going
       in a different direction, so throw out all the om-sync stuff
       and leave only edn-xhr for convenience. There probably is a more
       appropriate wrapper for XHR, though.
    
     - Put in some channels. These are from the Om Advanced Tutorial and I'm
       not sure if I'll need them, but I leave them there, just in case. I
       considered using the Request Channel pattern, but then came to the
       conclusion that it just adds a level of indirection, which I don't
       need for now. Therefore keep things small and call process-input
       directly. (Instead of putting a request on the channel and having a
       request handler call the equivalent of process-input.)
    
     - The ref-cursor pattern is also from the Om Advanced Tutorial. I'm not
       sure whether using it is appropriate in my case or not. There are too
       many choices and too few guidelines for passing around data in Om.
    
     - I'm often passing nil to om/build. This is a result of the ref-cursor
       pattern: you don't pass around cursors anymore. I'll have to see if
       there is a shortcut to om/build without a second argument or
       something like that. Maybe I'm just misunderstanding some part of the
       system.
    
     - Search strings are not treated in any way, but there seems to be some
       sort of encoding and decoding going on under the hood, so that spaces
       and other not URI-conform characters come out well at the other end.
       However, that's not what other search engines do. I'll have to think
       about how to do this properly.
    
    Sorry for the large commit, but if I split it into smaller pieces, the
    system wouldn't have worked in between and that's also not optimal.

--- src/clj/theatralia/routes.clj
+++ src/clj/theatralia/routes.clj
@@ -14,38 +14,19 @@
    :headers {"Content-Type" "application/edn"}
    :body (pr-str data)})
 
-(defn last-input [conn]
-  "Returns a tuple of the ID of the entity with the :last-input/text attribute
-  and the value of that attribute."
-  (first (d/q '[:find ?e ?t :where [?e :last-input/text ?t]]
-              (d/db conn))))
-
-(defn init [conn]
-  (let [[_ text] (last-input conn)
-        default "Hello World!"]
-    (when (nil? text)
-      @(d/transact conn
-                   [[:db/add #db/id[:db.part/user] :last-input/text default]]))
-    (generate-response
-      {:last-input {:url "/last-input"
-                    :coll {:text (or text default)}}})))
-
-(defn update-last-input
-  "Takes a request map with a :text key and updates the newest entity with the
-  attribute :last-input/text."
-  [{:keys [text]} conn]
-  (let [[eid _] (last-input conn)]
-    @(d/transact conn [[:db/add eid :last-input/text text]]))
-  (generate-response {:status :ok}))
+(defn search-for [conn s]
+  (generate-response
+    (sort-by #(nth % 2) >
+             (d/q '[:find ?e ?t ?sc
+                    :in $ ?s
+                    :where [(fulltext $ :material/title ?s) [[?e ?t _ ?sc]]]]
+                  (d/db conn) s))))
 
 (defn make-handler [routes]
   (-> (let [conn (:conn (:database routes))]
         (cj/routes
           (cj/GET "/" [] (wp/index))
-          (cj/GET "/init" [] (init conn))
-          (cj/GET "/last-input" [] (second (last-input conn)))
-          (cj/PUT "/last-input" {params :edn-params}
-                  (update-last-input params conn))
+          (cj/GET "/gq/:s" [s] (search-for conn s))
           (cj/GET "/main.css" [] (wp/main-css))
           (route/resources "/")
           (route/not-found "<h1>Page not found</h1>")))
--- src/cljs/theatralia/core.cljs
+++ src/cljs/theatralia/core.cljs
@@ -4,8 +4,7 @@
             [goog.dom :as gdom]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
-            [om-sync.core :refer [om-sync]]
-            [om-sync.util :refer [tx-tag edn-xhr]]
+            [om-sync.util :refer [edn-xhr]]
             [kioo.om :as kiom]
             kioo.util ; so that kioo/component won't cause warnings
             [kioo.core :as kioo :include-macros true]))
@@ -13,74 +12,85 @@
 ;;; Credits:
 ;;;  - https://github.com/swannodette/om
 ;;;  - https://github.com/swannodette/om/wiki/Basic-Tutorial
+;;;  - https://github.com/omcljs/om/wiki/Advanced-Tutorial
 ;;;  - https://github.com/ckirkendall/kioo
 ;;;  - https://github.com/ckirkendall/todomvc/blob/gh-pages/labs/architecture-examples/kioo/src/todomvc/app.cljs
 
 (enable-console-print!)
 
 (def app-state
-  (atom {}))
+  (atom {:search-result []}))
+
+(defn search-result []
+  (om/ref-cursor (:search-result (om/root-cursor app-state))))
 
 (defn handle-change [event owner {:keys [text]}]
   (om/set-state! owner :text (.. event -target -value)))
 
-(defn process-input [owner last-input]
-  (let [input (.-value (om/get-node owner "misc-input"))]
+(defn process-input [owner]
+  (let [input (.-value (om/get-node owner "search-input"))]
     (when input
-      (om/transact! last-input :text (fn [_] input) :update)
-      (om/set-state! owner :text ""))))
+      (edn-xhr {:method :get
+                :url (str "/gq/" input)
+                :on-complete (fn [res]
+                               (om/transact! (search-result) #(vec res)))
+                :on-error (fn [{:keys [error]}] (println "Error:" error))}))))
 
-(defn form-view [last-input owner]
+(defn result-item [[id title score] owner]
+  (om/component
+    (kioo/component "templates/sandbox.html"
+      [:#search-results :> :ol :> first-child]
+      {[:li] (kioo/content title)})))
+
+(defn result-view [_ owner]
+  (reify
+    om/IRender
+    (render [this]
+      (let [rs (om/observe owner (search-result))]
+        (kioo/component "templates/sandbox.html" [:#search-results]
+          {[:ol] (kioo/content (om/build-all result-item rs))})))))
+
+(defn search-view [_ owner]
   (reify
     om/IInitState
     (init-state [_] {:text ""})
 
     om/IRenderState
     (render-state [this local-state]
-      (kioo/component "templates/bootstrap-test.html" [:#test-form]
-        {[:#miscInput] (kioo/set-attr
-                         :value (:text local-state)
-                         :onChange #(handle-change % owner local-state)
-                         :onKeyDown #(when (= (.-key %) "Enter")
-                                       (process-input owner last-input)))
+      (kioo/component "templates/sandbox.html" [:#search-field]
+        {[:#searchInput] (kioo/set-attr
+                           :value (:text local-state)
+                           :onChange #(handle-change % owner local-state)
+                           :onKeyDown #(when (= (.-key %) "Enter")
+                                         (process-input owner)))
          [:#submit] (kioo/set-attr
-                      :onClick #(process-input owner last-input))}))
+                      :onClick #(process-input owner))}))
 
     om/IDidMount
     (did-mount [_]
-      (.focus (om/get-node owner "misc-input")))))
+      (.focus (om/get-node owner "search-input")))))
 
-(defn io-view [last-input owner]
+(defn sandbox-view [_ owner]
   (reify
     om/IRender
     (render [_]
-      (kioo/component "templates/bootstrap-test.html"
-        {[:.page-header :> :h1] (kiom/content (:text last-input))
-         [:#test-form] (kiom/substitute (om/build form-view last-input))}))))
+      (kioo/component "templates/sandbox.html"
+        {[:#search-field]
+         (kioo/substitute (om/build search-view nil))
+
+         [:#search-results]
+         (kioo/substitute (om/build result-view nil))}))))
 
 (defn app-view [app owner]
   (reify
     om/IRender
     (render [_]
-      (om/build
-        om-sync
-        (:last-input app)
-        {:opts
-         {:view io-view
-          :filter (comp #{:update} tx-tag)
-          :on-success (fn [res tx-data] (println res))
-          :on-error (fn [err tx-data] (println (str "Error: " err)))}}))))
+      (om/build sandbox-view nil))))
 
 (let [tx-chan (chan)
       tx-pub-chan (async/pub tx-chan (fn [_] :txs))]
-  (edn-xhr
-    {:method :get
-     :url "/init"
-     :on-complete
-     (fn [res]
-       (reset! app-state res)
-       (om/root app-view app-state
-                {:target (gdom/getElement "app")
-                 :shared {:tx-chan tx-pub-chan}
-                 :tx-listen (fn [tx-data root-cursor]
-                              (put! tx-chan [tx-data root-cursor]))}))}))
+  (om/root app-view app-state
+           {:target (gdom/getElement "app")
+            :shared {:tx-chan tx-pub-chan}
+            :tx-listen (fn [tx-data root-cursor]
+                         (put! tx-chan [tx-data root-cursor]))}))
