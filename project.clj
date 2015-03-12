25bb8ee8c4e92e394599d81af6aca7b5058db242
Richard Möhn <richard.moehn@posteo.de>
2015-03-12 13:06:58 +0100

    React to React warnings
    React had produced some warnings, which had been due partly to my
    mistakes, partly to missing things in Kioo.
    
     - Add a closing tag to <textarea>. – I had thought it was a void
       element. Now it's right and even has the placeholder, which hadn't
       worked because of my mistake.
    
     - Add an argument to the search input field's autofocus attribute.
       autofocus is a boolean attribute and as such doesn't require an
       argument. However, Kioo stumbled over that, so change it to the form
       with argument. I'm not sure whether it does what it's supposed to do:
       in Chromium the field is autofocused, in Firefox not. Although when I
       inspect the element in Chromium, it doesn't have an autofocus
       attribute. Strange stuff…
    
     - Change to the snapshot version of the Kioo dependency. This is a fix
       only for my local setup, since the version of Kioo with my changes in
       it isn't on Clojars yet. I hope Creighton will deploy it soon, so
       that I can put a proper unambiguous and usable-by-everyone snapshot
       dependency in the project.clj.

--- project.clj
+++ project.clj
@@ -18,7 +18,7 @@
                  [compojure "1.2.1"]
                  [garden "1.2.5"]
                  [enlive "1.1.5"]
-                 [kioo "0.4.0"]
+                 [kioo "0.4.1-SNAPSHOT"]
                  [com.datomic/datomic-pro "0.9.5130" :exclusions [joda-time]]]
 
   :node-dependencies [[source-map-support "0.2.8"]]
