7cc234581c0a2cd2efe8b99b957192442cfbebf9
Richard Möhn <richard.moehn@posteo.de>
2015-04-09 11:48:05 +0900

    Correct Datomic authentication for Travis
    I hadn't paid attention to the fact that non-GPG authentication uses a
    different mechanism in Leiningen. Luckily environment variables work,
    too. Adjust Travis and Leiningen config accordingly.

--- project.clj
+++ project.clj
@@ -9,7 +9,8 @@
             :url "http://opensource.org/licenses/MIT"}
 
   :repositories {"my.datomic.com" {:url "https://my.datomic.com/repo"
-                                   :creds :gpg}}
+                                   :username [:gpg :env/datomic_username]
+                                   :passphrase [:gpg :env/datomic_passphrase]}}
 
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2913"]
