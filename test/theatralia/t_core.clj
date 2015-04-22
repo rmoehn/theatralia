2809fbb3688e75542809ee4ff19d7e3b660f702a
Richard Möhn <richard.moehn@posteo.de>
2015-04-22 12:12:02 +0900

    Add Midje checker for cooperation with test.check
    I'm not yet sure where to put it, though. Does test support code belong
    in src or test?

--- test/theatralia/t_core.clj
+++ test/theatralia/t_core.clj
@@ -5,18 +5,23 @@
             [clojure.test.check.properties :as prop]
             [theatralia.core :refer [first-element]]))
 
-;; Credits: http://zotskolf.nl/2014/11/10/testcheckbasics.html
-(def prop-first-element-returns-first-element
-  (prop/for-all [fst gen/simple-type
-                 v (gen/vector gen/simple-type)]
-    (= fst (first-element (cons fst v) :default))))
+;;; Credits: http://zotskolf.nl/2014/11/10/testcheckbasics.html
+
+(defn quick-check [times]
+  (fn [prop]
+    ((just {:result true :num-tests times :seed anything})
+     (tc/quick-check times prop))))
 
 (facts "about `first-element`"
   (fact "it normally returns the first element"
     (first-element [1 2 3] :default) => 1
-    (first-element '(1 2 3) :default) => 1
-    (tc/quick-check 100 prop-first-element-returns-first-element)
-        => (just {:result true :num-tests 100 :seed anything}))
+    (first-element '(1 2 3) :default) => 1)
+
+  (fact "it returns the first element for lots of vectors"
+    (prop/for-all [fst gen/simple-type
+                   v (gen/vector gen/simple-type)]
+      (= fst (first-element (cons fst v) :default)))
+      => (quick-check 100))
 
   ;; I'm a little unsure how Clojure types map onto the Lisp I'm used to.
   (fact "default value is returned for empty sequences"
