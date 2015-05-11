68a8041de88d16e527d338430a5019eaad08c029
Richard Möhn <richard.moehn@posteo.de>
2015-05-11 20:49:23 +0900

    Clean up tests for add-tags-txd
    This is what I set out to do some commits ago, but then got interrupted
    by other considerations. Pull out some structure from the properties.
    Add comments. Rearrange the definitions.
    
    Note that I started using all uppercase for procedure arguments (already
    in one of the preceding commits). [1] hinted me to this convention from
    Emacs Lisp and I find it quite helpful. Especially when I want to use a
    word both as an argument name and as a regular word.
    
    Pulling out common structure meant writing a macro. Of course I could
    have made it a function, but that wouldn't have been as pretty. Alas, I
    can't use it for the fact about "transacting doesn't modify", since this
    one has a slightly different setup than the others.
    
    [1] https://groups.google.com/d/msg/clojure/kBXoYuL2bP0/5QQSrN1lrNkJ

--- test/clj/theatralia/database/t_txd_gen.clj
+++ test/clj/theatralia/database/t_txd_gen.clj
@@ -1,5 +1,7 @@
 (ns theatralia.database.t-txd-gen
   (:require [midje.sweet :refer :all]
+            [clojure.data :refer [diff]]
+            [clojure.pprint :refer [pprint]]
             [clojure.test.check :as tc]
             [clojure.test.check.generators :as gen]
             [clojure.test.check.properties :as prop]
@@ -8,50 +10,116 @@
             [datomic.api :as d]
             [theatralia.database.canned-queries :as qcan]
             [theatralia.database.component :as db-component]
-            [theatralia.database.txd-gen :as nut])) ; namespace under test
+            [theatralia.database.txd-gen :as txd-gen]))
+
+;;;; General purpose helpers
+
+(defn contains-all?
+  "Returns true iff all elements from seq NEEDLES are contained in seq
+  HAYSTACK."
+  [haystack needles]
+  (every? (set haystack) needles))
+
+;;;; Auxiliary functions for testing against Datomic
 
 ;;; Credits: http://yellerapp.com/posts/2014-05-07-testing-datomic.html
 
-(defn empty-db-conn
+(defn fresh-db
+  "Returns a db that contains only the example data."
   []
   (-> (db-component/make-database "datomic:mem://theatralia-test")
       component/start
-      :conn))
+      :conn
+      d/db))
 
-(defn empty-db
-  []
-  (d/db (empty-db-conn)))
-
-(defn suppose [db txd]
+(defn suppose
+  "Returns a db that looks as if TXD had been transacted into DB."
+  [db txd]
   (:db-after (d/with db txd)))
 
-(defn contains-all? [haystack needles]
-  (every? (set haystack) needles))
+;;;; Sugar for easier testing of txd-gen/add-tags-txd
+
+(defn add-tags-txd-only
+  "Like txd-gen/add-tags-txd, but returns only the second element of the tuple
+  returned by the add-tags-txd."
+  [& args]
+  ((apply txd-gen/add-tags-txd args) 1))
+
+(defmacro tags-txd-prop
+  "Template for shortening the definition of a property of txd-gen/add-tags-txd
+  slighly. BODY will be evaluated in a context where ARGS are filled as follows:
+
+    0 (tags)      – a generator generating a vector of strings to be used as
+                    tags
+    1 (owner-eid) – the eid of the user for whom we're adding tags
+    2 (db-before) - the database value before adding TAGS
+    3 (db-after)  – the database value after adding TAGS"
+  [args & body]
+  `(let [db-before# (fresh-db)
+         owner-eid# (qcan/username->eid db-before# "sandbox")
+         ~(subvec args 1 3) [owner-eid# db-before#]]
+     (prop/for-all [tags# (gen/vector gen/string)]
+       (let [db-after# (suppose db-before#
+                                (add-tags-txd-only db-before# tags# owner-eid#))
+             ~(args 0) tags#
+             ~(args 3) db-after#]
+         ~@body))))
+
+;;;; Tests for txd-gen/add-tags-txd
 
 (facts "about add-tags-txd"
   (fact "After transacting the results, all tags should be present in DB."
-    (let [db (empty-db)
-          owner-eid (qcan/username->eid db "sandbox")]
-      (prop/for-all [tags (gen/vector gen/string-ascii)]
-        (let [db-after (suppose db ((nut/add-tags-txd db tags owner-eid) 1))
-              tags-after (qcan/tags-of-user-eid db-after owner-eid)]
-          (contains-all? tags-after tags))))
+    (tags-txd-prop [tags owner-eid db-before db-after]
+      (let [tags-after (qcan/tags-of-user-eid db-after owner-eid)]
+        (contains-all? tags-after tags)))
+    => (quick-check 100))
+
+  (fact "Transacting the generated data doesn't modify pre-existing tags."
+    (let [db-0 (fresh-db)
+          owner-eid (qcan/username->eid db-0 "sandbox")]
+      (prop/for-all [tags-1 (gen/vector gen/string)
+                     tags-2 (gen/vector gen/string)]
+        (let [db-1 (suppose db-0 (add-tags-txd-only db-0 tags-1 owner-eid))
+              tag-eids-1 (d/q '[:find [?e ...] :where [?e :tag/text _]] db-1)
+              db-2 (suppose db-1 (add-tags-txd-only db-1 tags-2 owner-eid))
+              [tag-info-1 tag-info-2] (mapv #(d/pull-many % '[*] tag-eids-1)
+                                            [db-1 db-2])]
+          (= tag-info-1 tag-info-2))))
     => (quick-check 100))
 
   (fact "After transacting once, the result of the second run should be empty."
-    (let [db (empty-db)
-          owner-eid (qcan/username->eid db "sandbox")]
-      (prop/for-all [tags (gen/vector gen/string)]
-        (let [db-after (suppose db ((nut/add-tags-txd db tags owner-eid) 1))
-              new-txd ((nut/add-tags-txd db-after tags owner-eid) 1)]
-          (empty? new-txd))))
+    (tags-txd-prop [tags owner-eid db-before db-after]
+      (let [new-txd (add-tags-txd-only db-after tags owner-eid)]
+        (empty? new-txd)))
     => (quick-check 100))
 
   (fact "There should be no two tags with the same text and owner-eid."
-    (let [db (empty-db)
-          owner-eid (qcan/username->eid db "sandbox")]
-      (prop/for-all [tags (gen/vector gen/string)]
-        (let [db-after (suppose db ((nut/add-tags-txd db tags owner-eid) 1))
-              tags-after (qcan/tags-of-user-eid db-after owner-eid)]
-          (apply distinct? tags-after))))
+    (tags-txd-prop [tags owner-eid db-before db-after]
+      (let [tags-after (qcan/tags-of-user-eid db-after owner-eid)]
+        (apply distinct? tags-after)))
     => (quick-check 100)))
+
+(comment
+
+  (require '[theatralia.database.t-txd-gen :as t])
+  (require '[theatralia.database.canned-queries :as qcan])
+  (require '[theatralia.database.txd-gen :as txd-gen])
+
+  (def db-0 (t/fresh-db))
+  (def oeid (qcan/username->eid db-0 "sandbox"))
+  (def res-1 (txd-gen/add-tags-txd db-0 ["a"] oeid))
+
+
+  )
+
+  ; add-material-txd
+  ; Properties:
+  ;  - None should have nil? for a value.
+  ;  - Hm, this one is pretty borinrc/clj/theatralia/routes.clj
+  ;  - What do we want to do if same material transacted twice?
+  ;  - Tags already present shouldn't be modified.
+  ;  - Shouldn't add more tags than we want.
+  ;  - Make sure that when there are some tags already in the database and our
+  ;    list of tags contains some of these, the ones that are contained are left
+  ;    in peace and the extra ones are added.
+  ;  - What about duplicate tags in tags?
