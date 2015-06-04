721317f668c1b715325bf1660f2fadede0e1d1cb
Richard Möhn <richard.moehn@posteo.de>
2015-06-04 16:29:06 +0900

    Try to emulate re-frame in Om
    I read through the re-frame documentation and found it appealing. So
    appealing that I want Theatralia to follow that pattern. I tried to
    coerce Om into using it and it nearly succumbed. However, the concept of
    signals cannot be expressed in a sensible way. – We have to thread the
    owner through the call chain and this can get rather ugly. – Some more
    of my considerations are written down in the big comment. So, I'll have
    to abandon my Om and look for new one.

--- src/cljs/theatralia/core.cljs
+++ src/cljs/theatralia/core.cljs
@@ -6,6 +6,7 @@
   it is loaded."
   (:require-macros [cljs.core.async.macros :refer [go]])
   (:require [cljs.core.async :as async :refer [put! chan alts!]]
+            [clojure.string :as string]
             [goog.dom :as gdom]
             [om.core :as om :include-macros true]
             [om.dom :as dom :include-macros true]
@@ -36,19 +37,69 @@
 (def app-state
   (atom {:search-result []}))
 
+(def events (chan))
+
 (defn search-result []
   (om/ref-cursor (:search-result (om/root-cursor app-state))))
 
+(defn rc-top-result []
+  (let [res (first (search-result))]
+    (println (type res))
+    res))
+
+(defn rc-processed-top-result []
+  (when-let [[i t s] (rc-top-result)]
+    [i (string/upper-case t) s]))
+
+(defn maybe-observe [owner maybe-ref-cursor]
+  (when maybe-ref-cursor (om/observe owner maybe-ref-cursor)))
+
+(defn top-result []
+  (first (to-observe)))
+
+; - Want to create a signal from a signal.
+; - In this case a (search-result) is a signal. I can observe it.
+; - It should be possible to create a signal B from signal A without observing
+;   signal A. Only want to observe signal B.
+; - What is the problem? In search-result om/ref-cursor always gets a cursor to
+;   a vector.
+; - If I want to make a signal from this that wants something out of this
+;   vector, the something has to be a data structure itself in order to still be
+;   a signal. We can't get out primitives, because they can't be signalic.
+; - That's the general problem. Primitives can't be signals. This introduces two
+;   issues:
+;     - Whenever we want a primitive to be a signal, we have to wrap it before
+;       putting it into the app-state and unwrap it after taking it out. This
+;       makes the code ugly. It could be mitigated some Lispy cunning, but I
+;       have a feeling that there are quite a lot of places that would need
+;       fixing. Not justifiable.
+;     - Even if we have collections of non-primitives, these collections can be
+;       empty and operations applied to them (like get) now return a primitive:
+;       nil. Having to wrap nil would require to mitigate even more things,
+;       since nil is used as a special value in a number of places and [nil]
+;       can't work there.
+; - How often do we need primitives that are signals?
+;     - Everything that is some sort of a label would be a primitive signal. For
+;       example, the value of an input field (locals made global) is a primitive
+;       signal.
+;     - See the second subitem of the previous item.
+; - All in all a pretty sad diagnosis. Can this be fixed? Can Om have signals of
+;   everything? Hmm, as long as we pass the owner all the way down, it's fine.
+
+(defn processed-top-result [owner]
+  (when-let [[i t s] (top-result owner)]
+    [i (string/upper-case t) s]))
+
+(defn processed-search-result [owner]
+  (map (fn [[i t s]] [i (string/upper-case t) s])
+       (om/observe owner (search-result))))
+
 ;;;; Input field part of search
 
-(defn process-input [owner]
-  (let [input (.-value (om/get-node owner "search-input"))]
-    (when input
-      (edn-xhr {:method :get
-                :url (str "/gq/" input)
-                :on-complete (fn [res]
-                               (om/transact! (search-result) #(vec res)))
-                :on-error print-error}))))
+(defn process-search [owner]
+  (when-let [input (.-value (om/get-node owner "search-input"))]
+    (put! events [:perform-search
+                  (.-value (om/get-node owner "search-input"))])))
 
 (defn search-view [_ owner]
   (reify
@@ -58,13 +109,12 @@
     om/IRenderState
     (render-state [this local-state]
       (kioo/component "templates/sandbox.html" [:#search-field]
-        {[:#searchInput] (kioo/set-attr
-                           :value (:text local-state)
-                           :onChange #(handle-change % :text owner)
-                           :onKeyDown #(when (= (.-key %) "Enter")
-                                         (process-input owner)))
-         [:#submit] (kioo/set-attr
-                      :onClick #(process-input owner))}))))
+        {[:#searchInput]
+         (kioo/set-attr
+           :value (:text local-state)
+           :onChange #(handle-change % :text owner)
+           :onKeyDown #(when (= (.-key %) "Enter") (process-search owner)))
+         [:#submit] (kioo/set-attr :onClick #(process-search owner))}))))
 
 ;;;; Result part of search
 
@@ -134,10 +184,30 @@
     (render [_]
       (om/build sandbox-view nil))))
 
-(let [tx-chan (chan)
-      tx-pub-chan (async/pub tx-chan (fn [_] :txs))]
-  (om/root app-view app-state
-           {:target (gdom/getElement "app")
-            :shared {:tx-chan tx-pub-chan}
-            :tx-listen (fn [tx-data root-cursor]
-                         (put! tx-chan [tx-data root-cursor]))}))
+(defmulti route (fn [app-state [ev-key & data]] ev-key))
+
+(defmethod route :perform-search [app-state [_ s]]
+  (edn-xhr {:method :get
+            :url (str "/gq/" s)
+            :on-complete (fn [res]
+                           (put! events [:got-search-results (vec res)]))
+            :on-error print-error})
+  app-state)
+
+(defmethod route :got-search-results [app-state [_ r]]
+  (println app-state)
+  (assoc app-state :search-result r))
+
+(defmethod route :default [app-state x]
+  (println "Got request" x)
+  app-state)
+
+(defn handle [event]
+  (println app-state (om/root-cursor app-state))
+  (om/transact! (om/root-cursor app-state) #(route % event))
+  (println app-state (om/root-cursor app-state)))
+
+(go (while true (handle (<! events))))
+
+(om/root app-view app-state
+         {:target (gdom/getElement "app")})
