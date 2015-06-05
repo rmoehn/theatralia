de9b17a5836fd1936685069ed1ff546f828ee8b2
Richard Möhn <richard.moehn@posteo.de>
2015-06-05 13:57:13 +0900

    Clean up project.clj
     - Update dependencies. – First and foremost Clojure and ClojureScript.
       But also all the others except Garden. – Garden 1.2.6 has some
       issues.
    
     - Remove source-map-support dependency, lein-npm and ClojureScript REPL
       stuff. – I had put in these things after reading some of David
       Nolen's blog posts like "The Old Way, The New Way". However,
       after one of the last ClojureScript releases the blog posts became
       outdated. It could well be that the things I removed were still
       relevant, but I wanted to have something of a clean slate before I
       start to try and make Figwheel work.
    
     - Put ClojureScript outputs on :clean-targets. – This is possible,
       because I added the cljsbuild hook some time ago, so that the
       ClojureScript gets compiled after cleaning and before assembling the
       final JAR file.
    
     - Rename the …-incomplete.jar to …-slim.jar. – This appears to be
       the conventional name.
    
     - Remove :notify-command. – It didn't notify me of warnings, so I
       had to check the output everytime anyway.
    
     - Remove comments about target/classes. – I've never found that I need
       those in the :source-paths.

--- project.clj
+++ project.clj
@@ -12,65 +12,55 @@
                                    :username [:gpg :env/datomic_username]
                                    :password [:gpg :env/datomic_passphrase]}}
 
-  :dependencies [[org.clojure/clojure "1.6.0"]
-                 [org.clojure/clojurescript "0.0-3196"]
-                 [medley "0.5.5"]
-                 [com.stuartsierra/component "0.2.2"]
+  :dependencies [[org.clojure/clojure "1.7.0-RC1"]
+                 [org.clojure/clojurescript "0.0-3308"]
+                 [medley "0.6.0"]
+                 [com.stuartsierra/component "0.2.3"]
                  [com.taoensso/timbre "3.4.0"]
                  [slingshot "0.12.2"]
                  [dire "0.5.3"]
                  [org.clojure/core.async "0.1.346.0-17112a-alpha"]
                  [om "0.8.0-rc1"]
                  [om-sync "0.1.1"]
-                 [ring "1.3.1"]
-                 [fogus/ring-edn "0.2.0"]
-                 [compojure "1.2.1"]
+                 [ring "1.3.2"]
+                 [fogus/ring-edn "0.3.0"]
+                 [compojure "1.3.4"]
                  [garden "1.2.5"]
                  [enlive "1.1.5"]
                  [kioo "0.4.1-20150314.121103-8" :exclusions [org.clojure/clojure]]
                  [com.datomic/datomic-pro "0.9.5130" :exclusions [joda-time]]
                  [beckon "0.1.1"]]
 
-  :node-dependencies [[source-map-support "0.2.8"]]
-
-  :plugins [[lein-ring "0.8.13"]
-            [lein-cljsbuild "1.0.5"]
-            [lein-npm "0.4.0"]]
+  :plugins [[lein-ring "0.9.4"]
+            [lein-cljsbuild "1.0.6"]]
 
   :source-paths ["src/clj" "src/cljs"]
   :test-paths ["test/clj"]
   :resource-paths ["resources"]
 
-  :clean-targets  [:target-path
-
-                  ;; The following can't be :clean-targets, because they would
-                  ;; get deleted before they could be packed into the uberjar.
-                  ;; Delete them manually if you need.
-                  ;"resources/public/js/out"
-                  ;"resources/public/js/repl-out"
-                  ;"resources/public/js/main.js"
-                  ;"resources/public/js/main.min.js"
-                  ]
+  :clean-targets  ^{:protect false} [:target-path
+                                     "resources/public/js/out"
+                                     "resources/public/js/main.js"
+                                     "resources/public/js/main.min.js"]
 
   :profiles {:dev {:source-paths ["dev"]
                    :dependencies
                    [[midje "1.6.3" :exclusions [org.clojure/clojure]]
                     [org.clojure/test.check "0.7.0"]
-                    [org.clojure/tools.namespace "0.2.7"]]
+                    [org.clojure/tools.namespace "0.2.10"]]
                    :plugins
                    [[lein-midje "3.1.3"]]}
              :uberjar {:main theatralia.core
                        :aot [theatralia.core]}}
 
-  :jar-name "theatralia-%s-incomplete.jar"
+  :jar-name "theatralia-%s-slim.jar"
   :uberjar-name "theatralia-%s.jar"
 
   :hooks [leiningen.cljsbuild]
 
   :cljsbuild {
     :builds [{:id "dev"
-              :source-paths ["src/cljs"] ; Do we need target/classes?
-              :notify-command ["notify-send"]
+              :source-paths ["src/cljs"]
               :compiler {
                 :output-to "resources/public/js/main.js"
                 :output-dir "resources/public/js/out"
@@ -78,7 +68,7 @@
                 :cache-analysis true
                 :source-map true}}
              {:id "release"
-              :source-paths ["src/cljs"] ; Do we need target/classes?
+              :source-paths ["src/cljs"]
               :compiler {
                 :output-to "resources/public/js/main.min.js"
                 :pretty-print false
--- scripts/repl.clj
+++
@@ -1,9 +0,0 @@
-(require
-  '[cljs.repl :as repl]
-  '[cljs.repl.node :as node])
-
-(repl/repl* (node/repl-env)
-  {:output-dir "resources/public/js/repl-out"
-   :optimizations :none
-   :cache-analysis true
-   :source-map true})
