e0bcd07a1b77a9d17d462d32d8f55b6ae74b9734
Richard Möhn <richard.moehn@posteo.de>
2015-07-23 14:41:37 +0900

    TMP Add directory test-js from CicleCI unchanged

---
+++ test-js/require-karma.js
@@ -0,0 +1,13 @@
+goog.require('circle.karma');
+goog.require('frontend.sample_test');
+goog.require('frontend.components.project.test_common');
+goog.require('frontend.components.test_build_head');
+goog.require('frontend.components.test_documentation');
+goog.require('frontend.components.test_integrations');
+goog.require('frontend.components.test_org_settings');
+goog.require('frontend.components.test_statuspage');
+goog.require('frontend.components.test_build_head');
+goog.require('frontend.utils_test');
+goog.require('frontend.controllers.controls_test');
+goog.require('frontend.controllers.ws_test');
+goog.require('frontend.models.test_plan');
