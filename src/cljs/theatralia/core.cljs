8c032fa3b875dc930021cdb6dab07cd4f51ceb13
Richard Möhn <richard.moehn@posteo.de>
2015-08-27 16:52:42 +0900

    CLJS: Pull out subscription handlers from core
    See previous commit for the reason.

--- src/cljs/theatralia/core.cljs
+++ src/cljs/theatralia/core.cljs
@@ -4,8 +4,6 @@
   Defines an application with a part for searching and a part for adding
   materials. Installs it in the HTML element with ID \"app\" on the page where
   it is loaded."
-  (:require-macros [cljs.core.async.macros :refer [go]]
-                   [reagent.ratom :refer [reaction]])
   (:require [cljs.core.async :as async :refer [put! chan alts!]]
             [reagent.core :as reagent]
             [re-frame.core :as rf]
@@ -38,12 +36,6 @@
   sequences."
   [& args]
   (map vec (apply partition args)))
-
-(defn value
-  "Returns the value of the input field whose change caused the
-  TEXT-CHANGE-EVENT."
-  [text-change-event]
-  (-> text-change-event .-target .-value))
 
 (defn pull-single
   "Same as (safe-get (pull db [attr] eid) attr), i. e. it saves you from typing
@@ -131,39 +123,6 @@
 
 
 ;;;; Subscription handlers
-
-(defn get-scratch-entid
-  "Entity ID of the scratch area with SCRATCH-KEY as the value of its
-  :scratch/key attribute. Assumes that this scratch area already exists."
-  [conn [_ scratch-key]]
-  (let [r (tsky/bind '[:find ?e .
-                       :in $ ?key
-                       :where [?e :scratch/key ?key]]
-                     conn scratch-key)]
-    (assert @r)
-    r))
-(th-utils/register-sub* get-scratch-entid)
-
-(defn get-scratch-contents
-  "Contents of scratch area with entity ID SCRATCH-ENTID."
-  [conn [_ scratch-entid]]
-  (reaction (d/pull @conn '[*] scratch-entid)))
-(th-utils/register-sub* get-scratch-contents)
-
-(defn search-result
-  "Result of the material search."
-  [db []]
-  (tsky/bind '[:find [?rs ...]
-               :where [_ :search-result ?rs]]
-             db))
-(th-utils/register-sub* search-result)
-
-(defn tags
-  [db []]
-  (let [query-res (tsky/bind tags-query db)]
-    (reaction (sort-by first @query-res))))
-(th-utils/register-sub* tags)
-
 
 ;;;; Views
 
